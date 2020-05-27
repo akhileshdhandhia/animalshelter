e6c914c3f6954fd00d9fb3f39b7a829c86f20530
akhileshdhandhia <65979386+akhileshdhandhia@users.noreply.github.com>
2020-05-28 02:04:51 +0530

    view previous requests

--- index.php
+++ index.php
@@ -70,11 +70,14 @@ if (isset($_SESSION["email"])) {
 	<section class="callto-top-area section-gap">
 		<div class="container">
 			<div class="row justify-content-between callto-top-wrap pt-40 pb-40">
-				<div class="col-lg-8 callto-top-left">
+				<div class="col-lg-6 callto-top-left">
 					<h1>Please select your favourite pet</h1>
 				</div>
-				<div class="col-lg-4 callto-top-right">
+				<div class="col-lg-3 callto-top-right">
 					<a href="adopt.php" target="_blank" class="primary-btn">Fill Adoption Form</a>
+				</div>
+				<div class="col-lg-3 callto-top-right">
+					<a href="prevrecord.php" target="_blank" class="primary-btn">View previous enteries</a>
 				</div>
 			</div>
 		</div>
